c0be5648070a8721622f6518486f26230f6f0b41
xshche05 <xshche05@stud.fit.vutbr.cz>
2024-04-21 14:23:59 +0200

    Add properties and print

--- PcapPacket.cs
+++ PcapPacket.cs
@@ -16,9 +16,107 @@ public class PcapPacket
         _packetData = Packet.ParsePacket(packet.LinkLayerType, packet.Data);
         _packetType = PacketTypeParser.GetPacketType(_packetData);
     }
+
+    public bool Print()
+    {
+        if (_packetType == PacketType.Ignore)
+        {
+            return false;
+        }
+        Console.WriteLine($"timestamp: {TimeStamp}");
+        if (SrcMac != null)
+        {
+            Console.WriteLine($"src MAC: {SrcMac}");
+        }
+        if (DstMac != null)
+        {
+            Console.WriteLine($"dst MAC: {DstMac}");
+        }
+        Console.WriteLine($"frame length: {_packetData.Bytes.Length} bytes");
+        if (SrcIp != null)
+        {
+            Console.WriteLine($"src IP: {SrcIp}");
+        }
+        if (DstIp != null)
+        {
+            Console.WriteLine($"dst IP: {DstIp}");
+        }
+        if (SrcPort != null)
+        {
+            Console.WriteLine($"src port: {SrcPort}");
+        }
+        if (DstPort != null)
+        {
+            Console.WriteLine($"dst port: {DstPort}");
+        }
+        Console.WriteLine($"\n{HexDump}\n\n");
+        return true;
+    }
     
     private string TimeStamp => _rawCapture.Timeval.Date.ToString("yyyy-MM-dd HH:mm:ss.fffzzz");
-    
+    private string HexDump => GetHexDump(_packetData.Bytes);
+    public string? SrcMac
+    {
+        // todo add support for other link layer types
+        get
+        {
+            var ethernetPacket = _packetData.Extract<EthernetPacket>();
+            if (ethernetPacket != null)
+            {
+                return FormatMac(ethernetPacket.SourceHardwareAddress.ToString());
+            }
+            return null;
+        }
+    }
+    public string? DstMac
+    {
+        // todo add support for other link layer types
+        get
+        {
+            var ethernetPacket = _packetData.Extract<EthernetPacket>();
+            if (ethernetPacket != null)
+            {
+                return FormatMac(ethernetPacket.DestinationHardwareAddress.ToString());
+            }
+            return null;
+        }
+    }
+    public string? SrcIp => _packetData.Extract<IPPacket>()?.SourceAddress.ToString();
+    public string? DstIp => _packetData.Extract<IPPacket>()?.DestinationAddress.ToString();
+    public int? SrcPort
+    {
+        get
+        {
+            TcpPacket? tcpPacket = _packetData.Extract<TcpPacket>();
+            if (tcpPacket != null)
+            {
+                return tcpPacket.SourcePort;
+            }
+            UdpPacket? udpPacket = _packetData.Extract<UdpPacket>();
+            if (udpPacket != null)
+            {
+                return udpPacket.SourcePort;
+            }
+            return null;
+        }
+    }
+    public int? DstPort
+    {
+        get
+        {
+            TcpPacket? tcpPacket = _packetData.Extract<TcpPacket>();
+            if (tcpPacket != null)
+            {
+                return tcpPacket.DestinationPort;
+            }
+            UdpPacket? udpPacket = _packetData.Extract<UdpPacket>();
+            if (udpPacket != null)
+            {
+                return udpPacket.DestinationPort;
+            }
+            return null;
+        }
+    }
     private string? FormatMac(string? unformattedMac)
     {
         if (unformattedMac == null)
@@ -28,8 +126,7 @@ public class PcapPacket
         return string.Join(":", Enumerable.Range(0, 6)
             .Select(i => unformattedMac.Substring(i * 2, 2)));
     }
-    
-    private static string HexDump(byte[] data)
+    private static string GetHexDump(byte[] data)
     {
         // offset : bytes : ascii
         var hex = new StringBuilder();
