93a16522a4ac7207f2c385fc627cd0da16d26726
xshche05 <xshche05@stud.fit.vutbr.cz>
2024-04-21 14:24:53 +0200

    Error handling and capture entry point

--- Program.cs
+++ Program.cs
@@ -9,27 +9,38 @@ public static class Program
     public static LibPcapLiveDevice Interface { get; private set; } = null!;
     public static void Main(string[] args)
     {
-        // setup line breaks for console output
-        Console.Error.NewLine = "\n";
-        Console.Out.NewLine = "\n";
-        
-        _options = Options.Parse(args);
-        _options.Validate();
-
-        if (_options.Interface is null)
+        try
         {
-            // Print all available interfaces
-            var devices = CaptureDeviceList.Instance.ToList();
-            foreach (var device in devices)
+            // setup line breaks for console output
+            Console.Error.NewLine = "\n";
+            Console.Out.NewLine = "\n";
+
+            _options = Options.Parse(args);
+            _options.Validate();
+
+            if (_options.Interface is null)
             {
-                Console.WriteLine($"{device.Name} - {device.Description}");
+                // Print all available interfaces
+                var devices = CaptureDeviceList.Instance.ToList();
+                foreach (var device in devices)
+                {
+                    Console.WriteLine($"{device.Name} - {device.Description}");
+                }
+
+                // Exit the program
+                Environment.Exit(0);
             }
-            // Exit the program
-            Environment.Exit(0);
+
+            // Find the interface by name
+            var interfaceDevice = CaptureDeviceList.Instance.FirstOrDefault(d => d?.Name == _options.Interface, null);
+            Interface = interfaceDevice as LibPcapLiveDevice ??
+                        throw new ApplicationException("Interface not found, invalid name!");
+            Pcap.Capture();
         }
-        
-        // Find the interface by name
-        var interfaceDevice = CaptureDeviceList.Instance.FirstOrDefault(d => d?.Name == _options.Interface, null);
-        Interface = interfaceDevice as LibPcapLiveDevice ?? throw new ApplicationException("Interface not found, invalid name!");
+        catch (Exception e)
+        {
+            Console.Error.WriteLine(e.Message);
+            Environment.Exit(1);
+        }
     }
 }
