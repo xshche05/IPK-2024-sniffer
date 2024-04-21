4416a90000eec763758d64c50a7008a3d79dfd3b
xshche05 <xshche05@stud.fit.vutbr.cz>
2024-04-21 14:37:58 +0200

    Add help and validation

--- Options.cs
+++ Options.cs
@@ -2,6 +2,22 @@ namespace PacketSniffer;
 
 public class Options
 {
+    private readonly string _helpMessage =
+        "Usage: ./ipk-sniffer [-i interface | --interface interface] {-p|--port-source|--port-destination port [--tcp|-t] [--udp|-u]} [--arp] [--ndp] [--icmp4] [--icmp6] [--igmp] [--mld] {-n num}\n" +
+        "   -i or --interface: Specify interface to sniff. Without value, list active interfaces.\n" +
+        "   -t or --tcp: Display TCP segments.\n" +
+        "   -u or --udp: Display UDP datagrams.\n" +
+        "   -p: Filter TCP/UDP by port number.\n" +
+        "   --port-destination: Filter TCP/UDP by destination port.\n" +
+        "   --port-source: Filter TCP/UDP by source port.\n" +
+        "   --icmp4: Display only ICMPv4 packets.\n" +
+        "   --icmp6: Display only ICMPv6 echo request/response.\n" +
+        "   --arp: Display only ARP frames.\n" +
+        "   --ndp: Display only NDP packets (subset of ICMPv6).\n" +
+        "   --igmp: Display only IGMP packets.\n" +
+        "   --mld: Display only MLD packets (subset of ICMPv6).\n" +
+        "   -n 10: Specify number of packets to display (default: 1).\n" +
+        "   All protocols considered unless explicitly specified.";
     public string? Interface { get; set; } = null;
     public bool Tcp { get; set; }
     public bool Udp { get; set; }
@@ -30,6 +46,11 @@ public class Options
             var arg = args[i];
             switch (arg)
             {
+                case "-h":
+                case "--help":
+                    Console.WriteLine(options._helpMessage);
+                    Environment.Exit(0);
+                    break;
                 case "-i":
                 case "--interface":
                     if (i + 1 >= args.Length)
@@ -116,7 +137,12 @@ public class Options
     
     public void Validate()
     {
-        // todo validate options
+        if (Interface == null && _args.Length != 1)
+            throw new ApplicationException("not allowed options without interface specified");
+        if (Port != 0 && (PortSource != 0 || PortDestination != 0))
+            throw new ApplicationException("port filter with port and port source or port destination");
+        if ((Port != 0 || PortSource != 0 || PortDestination != 0) && !Tcp && !Udp)
+            throw new ApplicationException("port filter without TCP/UDP protocol");
     }
     
     public bool IsCapAll()
